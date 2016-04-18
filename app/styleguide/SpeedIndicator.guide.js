af37e09d29c012d03bfbd272fd47dbe65fe95874
kzverev <kostantin.zverev@gmail.com>
2016-04-18 13:41:18 +0200

    with examples
    Added an examples for each possible option.

--- app/styleguide/SpeedIndicator.guide.js
+++ app/styleguide/SpeedIndicator.guide.js
@@ -9,12 +9,20 @@ export default class SpeedIndicatorGuideComponent extends React.Component {
         return (
             <SGPage>
 
-                <SGSection title="SpeedIndicator - without properties">
-                    <SpeedIndicator />
+                <SGSection title="SpeedIndicator - with low speed property">
+                    <SpeedIndicator speed={14} />
                 </SGSection>
 
-                <SGSection title="SpeedIndicator - with speed property">
-                    <SpeedIndicator speed={77} />
+                <SGSection title="SpeedIndicator - with normal speed property">
+                    <SpeedIndicator speed={36} />
+                </SGSection>
+
+                <SGSection title="SpeedIndicator - with high speed property">
+                    <SpeedIndicator speed={67} />
+                </SGSection>
+
+                <SGSection title="SpeedIndicator - with dangerous speed property">
+                    <SpeedIndicator speed={90} />
                 </SGSection>
 
             </SGPage>
