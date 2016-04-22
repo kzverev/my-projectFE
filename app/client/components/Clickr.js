a5cbbe2b3ed389cbc659cf659bc63a95c844cc22
kzverev <kostantin.zverev@gmail.com>
2016-04-22 14:18:26 +0200

    click v3.0
    Background and restrictions added

--- app/client/components/Clickr.js
+++ app/client/components/Clickr.js
@@ -11,27 +11,51 @@ export default class Clickr extends React.Component {
     }
 
     handleClickPlus1 = (e) => {
-      this.setState({
-        count: this.state.count + 1
-      });
+      if (this.state.count > 19) {
+        this.setState({
+          count: 20
+        });
+      } else {
+        this.setState({
+          count: this.state.count + 1
+        });
+      }
     }
 
     handleClickPlus5 = (e) => {
-      this.setState({
-        count: this.state.count + 5
-      });
+      if(this.state.count > 15) {
+        this.setState({
+          count: 20
+        });
+      } else {
+        this.setState({
+          count: this.state.count + 5
+        });
+      }
     }
 
     handleClickminus1 = (e) => {
-      this.setState({
-        count: this.state.count - 1
-      });
+      if (this.state.count < -19) {
+        this.setState({
+          count: -20
+        });
+      } else {
+        this.setState({
+          count: this.state.count - 1
+        });
+      }
     }
 
     handleClickminus5 = (e) => {
-      this.setState({
-        count: this.state.count - 5
-      });
+      if(this.state.count < -15) {
+        this.setState({
+          count: -20
+        });
+      } else {
+        this.setState({
+          count: this.state.count - 5
+        });
+      }
     }
 
     render() {
@@ -39,13 +63,30 @@ export default class Clickr extends React.Component {
 
         var buttonStyle = {
           margin: '10',
+          display: 'inline-block',
         };
+
+        var counterStyle = {
+          backgroundColor: 'black',
+          display: 'inline-block',
+          width: '30',
+          height: '20',
+          textAlign: 'center',
+        };
+
+        if (count > 0) {
+          counterStyle.backgroundColor = 'green';
+        } else if (count < 0) {
+          counterStyle.backgroundColor = 'red'
+        }
 
         return (
             <div>
               <button style={buttonStyle} onClick={this.handleClickminus5}>-5</button>
               <button style={buttonStyle} onClick={this.handleClickminus1}>-1</button>
+              <div style={counterStyle}>
                 {count}
+              </div>
               <button style={buttonStyle} onClick={this.handleClickPlus1}>+1</button>
               <button style={buttonStyle} onClick={this.handleClickPlus5}>+5</button>
             </div>
